399960f7e6e829a24e11b5a9014e8650fd5c20aa
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-16 10:43:31 +0100

    j'ai termin controller du class cours

--- App/controller/CoursConroller.php
+++ App/controller/CoursConroller.php
@@ -3,6 +3,19 @@
 class CoursConroller
 {
     
+   public function ajouterCoursVedio($titre,$photoCouverture,$description,$idCategorie,$enseignat,
+                                      $nomberChapitre,$duree,$prix,$tags,$urlContenu,$id=null)
+   {
+    $coursVedio= new CoursVedio($titre,$photoCouverture,$description,$idCategorie,$enseignat,
+                                 $nomberChapitre,$duree,$prix,$tags,$urlContenu,$id=null);
+    $coursVedio->ajouter();
+   }
+   public function ajouterCoursText($titre,$photoCouverture,$description,$idCategorie,$enseignat,
+                                      $nomberChapitre,$duree,$prix,$tags,$texteContenu,$id=null)
+   {
+    $coursVedio= new CoursTexte($titre,$photoCouverture,$description,$idCategorie,$enseignat,
+                                 $nomberChapitre,$duree,$prix,$tags,$texteContenu,$id=null);
+    $coursVedio->ajouter();
+   }
 
-    
 }
