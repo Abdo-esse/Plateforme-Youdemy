e2d9c28d217fe4b5c658cf12b739debfd76705b1
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-20 09:54:57 +0100

    jai modefier  la function desearch dansla partie model et j'ai termu dens la prtie cintroller

--- App/class/Etudiant.php
+++ App/class/Etudiant.php
@@ -20,19 +20,5 @@ class Etudiant extends User
         Crud::createAction('inscription',$data);
     }
 
-    public function search($keyword){
-        $conn = Connexion::connexion();
-        $query = $conn->prepare("SELECT cours.id,cours.titre,cours.idEnseignant,cours.isPublier,cours.photoCouverture,cours.contenu,cours.description,cours.nomberChapitre,cours.duree,cours.prix,cours.dateCreation,cours.dateDelete,categories.name as categories ,users.name,GROUP_CONCAT(tags.name)as tags
-            from cours
-            JOIN users on users.id=cours.idEnseignant
-            join categories on categories.id=cours.idCategorie
-            join cours_tags on cours_tags.idCours=cours.id
-            join tags on tags.id=cours_tags.idTags
-            where cours.isPublier= true and cours.dateDelete IS NULL and (cours.titre like :keyword or categories like :keyword or tags like :keyword )
-            GROUP by cours.id
-            ORDER BY cours.id LIMIT 4");
-        $query->execute(array(":keyword" => '%' . $keyword . '%'));
-        $resulta= $query->fetchAll(PDO::FETCH_OBJ);
-        return json_encode($resulta);
-    }
+    
 }
---
+++ App/view/etudiant/search.php
@@ -0,0 +1,11 @@
+<?php 
+
+if(isset($_GET['keyword']) && !empty($_GET['keyword'])){
+    $cors=new EtudiantController();
+}
+
+
+
+
+
+?>
