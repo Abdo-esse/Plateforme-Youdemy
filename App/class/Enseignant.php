baa0a2197d622fb4be9625f496d264a563614700
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-21 21:59:32 +0100

    J'ai termin les methose de statistic dans model

--- App/class/Enseignant.php
+++ App/class/Enseignant.php
@@ -23,4 +23,31 @@ class Enseignant extends User
         $idEnseignant=Crud::createAction('users', $this->data);
         Crud::createAction('gestionEnseignants',["idEnseignant"=> $idEnseignant]) ;
  }
+
+ public function totalEtudiantInscrits(){
+  $conn = Connexion::connexion(); 
+  $sql="SELECT  count(inscription.idEtudiant)
+        from inscription
+        JOIN cours ON cours.id=inscription.idCours
+        join users on users.id=cours.idEnseignant
+        WHERE users.id=? ";
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($this->id);
+        return $stmt->fetch(PDO::FETCH_OBJ);
+
+ }
+ public function totalCoursIspulier(){
+  $conn = Connexion::connexion(); 
+  $sql="SELECT COUNT(id) AS nombre_cours
+        FROM cours
+        WHERE isPublier = ? AND idEnseignant = 16; ";
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($boolean,$this->id);
+        return $stmt->fetch(PDO::FETCH_OBJ);
+
+ }
+
+
+
+
 }
