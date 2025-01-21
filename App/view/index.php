df56273141d6a350d5952e51ab0d7e1483b893ec
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-21 15:53:33 +0100

    J'ai termin l'afichage des course dana l'admin

--- App/view/index.php
+++ App/view/index.php
@@ -266,7 +266,7 @@ $data=$cours->getDataController($currentPage);
         </svg>
         <span><?php echo $cours->nomberChapitre?> chapitres</span>
       </div>
-      <!-- Ajout du nombre d'étudiants -->
+     
       <div class="flex items-center text-gray-500 text-sm">
         <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
